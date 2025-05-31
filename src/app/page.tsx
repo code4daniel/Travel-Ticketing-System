1ddcbfbd0149d9de19afaafb0af88196569acf0c
Onyilo Daniel <code4daniel@gmail.com>
2025-05-31 14:09:58 -0700

    Resolved merge conflict in README.md

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center p-8 bg-indigo-400 text-white text-4xl">Hello, Developers</h1>
+    </div>
+  );
+}
